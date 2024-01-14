e21529f8b9910d1238256bfa0c78696104e3b31c
Adrián García Chavero <adrgarcha@alum.us.es>
2024-01-14 20:49:43 +0100

    Fix background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Background from "./Background";
 
 export default function App() {
   return (
-    <main className="h-screen">
+    <main className="relative h-screen w-full bg-slate-950 overflow-hidden">
       <Background />
     </main>
   )
