38242bbb36e7f779ff50d8c30d7f547234e91ead
renderer-design <manuelajgampa@gmail.com>
2024-01-22 21:33:50 +0100

    fun category fully completed

---
+++ src/components/DiscordMessage.jsx
@@ -0,0 +1,90 @@
+
+export const Componente = ({element, height, width}) =>{
+    return(<div
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        
+        marginRight: "10px", // Espacio entre la imagen y el contenido
+        overflow: "hidden", 
+        flexShrink: 0,
+      }}
+    >
+      <img
+        src={element} 
+        alt=""
+       
+      />
+    </div>);
+  }
+  
+  export default function DiscordMessage ({ msg, isBot, imagen, imagenHeight, imagenWidth }) {
+    const messageStyle = {
+      display: "flex",
+      flexDirection: "row", // Cambiado a fila para alinear la foto y el contenido
+      maxWidth: `400px`, // ajusta según tus necesidades
+      background: "#36393f", // color de fondo de Discord
+      padding: "10px",
+      borderRadius: "8px",
+      margin: "10px",
+      fontFamily: "gg sans Medium, sans-serif", // fuente utilizada por Discord
+    };
+  
+    const avatarStyle = {
+      width: "40px",
+      height: "40px",
+      borderRadius: "50%", // Hace que la imagen sea circular
+      marginRight: "10px", // Espacio entre la imagen y el contenido
+      overflow: "hidden", // Asegura que la imagen no sobresalga del círculo
+      flexShrink: 0,
+    };
+  
+    const authorContainerStyle = {
+      display: "flex",
+      alignItems: "center", // Alinea la etiqueta y el nombre verticalmente
+    };
+  
+    const authorStyle = {
+      color: "#7289da", // color del nombre de usuario en Discord
+      marginBottom: "-2px",
+    };
+  
+    const botTagStyle = {
+      background: "#7389da", // color morado de la etiqueta "BOT" en Discord
+      color: "#ffffff", // color del texto de la etiqueta
+      padding: "0px 5px",
+      borderRadius: "4px",
+      marginLeft: "5px", // Espacio entre la etiqueta y el nombre
+      fontSize: "12px",
+    };
+  
+    const contentStyle = {
+      color: "#ffffff", // color del texto en Discord
+    };
+  
+    return (
+      <div style={messageStyle}>
+        <div style={avatarStyle}>
+          <link
+            href="https://fonts.cdnfonts.com/css/gg-sans-2"
+            rel="stylesheet"
+          />
+          <img
+            src="/src/assets/mariwano-icon.png" // Reemplaza con la URL de la foto de perfil
+            alt="Avatar"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </div>
+        <div>
+          <div style={authorContainerStyle}>
+            <div style={authorStyle}>Mariwano</div>
+            {isBot && <div style={botTagStyle}>BOT</div>}
+          </div>
+          <div style={contentStyle}>{msg}
+          {imagen && <Componente element={imagen} height={imagenHeight} width={imagenWidth}/>}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
--- src/components/categories/FunCategory.jsx
+++ src/components/categories/FunCategory.jsx
@@ -1,44 +1,156 @@
 import CategorySection from "../CategorySection";
 import CategoryTitle from "../CategoryTtitle";
-import ryangoslingExample from "../../assets/commands/ryangosling.png";
-import padalustroExample from "../../assets/commands/padalustro.png";
-import fakeyouExample from "../../assets/commands/fakeyou.png";
-import kahootExample from "../../assets/commands/kahoot.png";
-import frasejokerExample from "../../assets/commands/frasejoker.png";
-import arabfunnyExample from "../../assets/commands/arabfunny.png";
-import arabfunnyExample2 from "../../assets/commands/arabfunny2.png";
-import crearmemeExample from "../../assets/commands/crearmeme.png";
+import DiscordMessage from "../DiscordMessage";
 
 export default function FunCategory() {
-    return (
-        <article className="flex flex-col gap-y-8 px-32 pt-10 pb-32">
-            <CategoryTitle title={"Diversión"}>
-                Categoría de los comandos buenos
-            </CategoryTitle>
-            <CategorySection command={"arabfunny"} params={"لا أحد"} firstImageUrl={arabfunnyExample} secondImageUrl={arabfunnyExample2}>
-                هذا الأمر يتضمن اقتراح إرسال رسائل باللغة العربية. يمكن تغيير نصوص الأوامر حسب الرغبة دون أن تتجاوز مليوني حر            </CategorySection>
-            <CategorySection command={"crearmeme"} params={"url, textoArriba, textoAbajo, efectos"} firstImageUrl={crearmemeExample}>
-                A partir de la url de una imagen (una que acabe en .jpg o .png o .gif) añade un texto inferior y uno superior. 
-                Existe la opción de añadir un efecto (esferizar, desenfocar, etc.). Envía el resultado de la imagen (por ahora no puede enviar gifs).
-            </CategorySection>
-            <CategorySection command={"fakeyou"} params={"voz, texto"} firstImageUrl={fakeyouExample}>
-                Envía un audio con la voz hecha con la plataforma de IA FakeYou de tu ídolo/youtuber/streamer favorito. 
-                Introduce a la persona que quieres que recite el texto en el parametro <i>voz</i> (el código coge el primero que encuentre) y el texto en el otro parámetro.
-            </CategorySection>
-            <CategorySection command={"frasejoker"} params={"add, all"} firstImageUrl={frasejokerExample} >
-                Envía una frase aleatoria digna de cita del Joker.
-            </CategorySection>
-            <CategorySection command={"kahoot"} params={"hardcore"} firstImageUrl={kahootExample} >
-                Un trivia de cultura general, si respondes en 15 segundos con la respuesta correcta, te corresponde
-                una recompensa en relación a la dificultad de la pregunta. En el modo <i>hardcore</i> sólo aparecen preguntas difíciles. Hay un límite de 5 preguntas diarias.
-            </CategorySection>
-            <CategorySection command={"padalustro"} params={"Ninguno"} firstImageUrl={padalustroExample} >
-                
-            </CategorySection>
-            <CategorySection command={"ryangosling"} params={"Ninguno"} firstImageUrl={ryangoslingExample} >
-                Envía una foto aleatoria de Ryan Gosling.
-            </CategorySection>
-            
-        </article>
-    )
+  return (
+    <article className="flex flex-col gap-y-8 px-32 pt-10 pb-32">
+      <CategoryTitle title={"Diversión"}>
+        Categoría de los comandos buenos
+      </CategoryTitle>
+
+      <CategorySection
+        command={"arabfunny"}
+        params={"لا أحد"}
+      >
+        هذا الأمر يتضمن اقتراح إرسال رسائل باللغة العربية. يمكن تغيير نصوص
+        الأوامر حسب الرغبة دون أن تتجاوز مليوني حر
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "70vh",
+        }}
+      >
+        <DiscordMessage
+          msg={
+            "الافتتاحية (الفاتحة) 1. بسم الله الرحمن الرحيم. 2. الحمد لله رب العالمين. 3. الرحمن الرحيم. 4. سيد يوم الدين. 5. إياك نعبد وإياك نستعين. 6. اهدنا إلى الصراط المستقيم. 7. صراط الذين أنعمت عليهم غير المغضوب عليهم ولا الضالين. 2. البقرة (البقرة) بسم الله الرحمن الرحيم 1. ألف، لام، ميم. 2. ذلك الكتاب لا ريب فيه هدى للمتقين. 3. الذين يؤمنون بالغيب ويقيمون الصلاة ومما رزقناهم ينفقون. 4. والذين يؤمنون بما أنزل إليك وما أنزل من قبلك وبالآخرة هم يوقنون. 5. أولئك على هدى من ربهم. هؤلاء هم الناجحون. 6. أما الذين كفروا سواء عليهم أأنذرتهم أم لم تنذرهم لا يؤمنون. 7. ختم الله على قلوبهم وعلى سمعهم وعلى أبصارهم غشاوة. ولهم عذاب شديد . 8. ومن الناس من يقول آمنا بالله واليوم الآخر وما هم بمؤمنين. 9. يخادعون الله والذين آمنوا وما يخدعون إلا أنفسهم وما يشعرون. 10. في قلوبهم مرض فزادهم الله مرضا. ولهم عذاب أليم بما كذبوا. 11. وإذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحون. 12. بل هم المفسدون ولكن لا يشعرون. 13. وإذا قيل لهم آمنوا كما آمن الناس قالوا أنؤمن كما آمن السفهاء؟ في"
+          }
+          isBot={true}
+        ></DiscordMessage>
+        <DiscordMessage
+          msg={
+            "😂مرحبًا، مساء الخير أيه😋ا الأوغاد، أناKERNEL😂 أدخن ذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل ل😂هم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا 🥵إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن م🥵صلحذا قيل لهم لا تفسدوا في الأرض قالوا إنما نحن مصلحسيجارة بيتونسيو ها🥵ئلة في الساعة 420 🤯بعد الظهر"
+          }
+          isBot={true}
+        ></DiscordMessage>
+      </div>
+      <CategorySection
+        command={"crearmeme"}
+        params={"url, textoArriba, textoAbajo, efectos"}
+      >
+        A partir de la url de una imagen (una que acabe en .jpg o .png o .gif)
+        añade un texto inferior y uno superior. Existe la opción de añadir un
+        efecto (esferizar, desenfocar, etc.). Envía el resultado de la imagen
+        (por ahora no puede enviar gifs).
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "20vh",
+        }}
+      >
+        <DiscordMessage msg={""} isBot={true} imagen={"/src/assets/commands/crearmeme2.png"} imagenHeight={60} imagenWidth={300}></DiscordMessage>
+      </div>
+      <CategorySection
+        command={"fakeyou"}
+        params={"voz, texto"}
+      >
+        Envía un audio con la voz hecha con la plataforma de IA FakeYou de tu
+        ídolo/youtuber/streamer favorito. Introduce a la persona que quieres que
+        recite el texto en el parametro <i>voz</i> (el código coge el primero
+        que encuentre) y el texto en el otro parámetro.
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "20vh",
+        }}
+      >
+        <DiscordMessage msg={"Cargando audio..."} isBot={true}></DiscordMessage>
+        <DiscordMessage msg={""} isBot={true} imagen={"/src/assets/commands/tts_audio.png"} imagenHeight={130} imagenWidth={300}>
+          
+        </DiscordMessage>
+      </div>
+      <CategorySection
+        command={"frasejoker"}
+        params={"add, all"}
+      >
+        Envía una frase aleatoria digna de cita del Joker.
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10vh",
+        }}
+      >
+        <DiscordMessage msg={"el que madruga se encuentra con todo cerrado😔🤙"} isBot={true}></DiscordMessage>
+        <DiscordMessage msg={"para mi el locomotor es sólo motor🥵"} isBot={true}></DiscordMessage>
+      </div>
+      <CategorySection
+        command={"kahoot"}
+        params={"hardcore"}
+      >
+        Un trivia de cultura general, si respondes en 15 segundos con la
+        respuesta correcta, te corresponde una recompensa en relación a la
+        dificultad de la pregunta. En el modo <i>hardcore</i> sólo aparecen
+        preguntas difíciles. Hay un límite de 5 preguntas diarias.
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "30vh",
+        }}
+      >
+        <DiscordMessage msg={""} isBot={true} imagen={"/src/assets/commands/kahoot.png"} imagenHeight={190} imagenWidth={320}></DiscordMessage>
+      </div>
+      <CategorySection
+        command={"padalustro"}
+        params={"Ninguno"}
+      ></CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "30vh",
+        }}
+      >
+        <DiscordMessage msg={"Tenga cuidado con el consumo de cannabis, un sólo porro contiene:\nCrotolamo\n· Aboreo\n· Acudo\n· Nifo\n· Uxiono\n· Trujo\n· Permatrago\n· Padalustro\n· Orbo\n· Tiro\n· Primo\n· Obo\n· Oplo\n· Crotofroto\n· Tampo\n· Timulo\n· Cupo\n· Combro"} isBot={true} ></DiscordMessage>
+      </div>
+      <CategorySection
+        command={"ryangosling"}
+        params={"Ninguno"}
+      >
+        Envía una foto aleatoria de Ryan Gosling.
+      </CategorySection>
+      <div
+        className="flex container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+        }}
+      >
+        <DiscordMessage msg={""} isBot={true} imagen={"/src/assets/commands/ryangosling.png"} imagenHeight={235} imagenWidth={300}></DiscordMessage>
+      </div>
+    </article>
+  );
 }
