acc2b4bfd76b5aa7c3cbc8fc2f50d44259864023
renderer-design <manuelajgampa@gmail.com>
2024-02-08 21:46:18 +0100

    link to navlink conversion issue

--- src/components/CommandCategory.jsx
+++ src/components/CommandCategory.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function CommandCategory({ route, icon, categoryName }) {
     return (
         <li className="m-3 rounded-xl transition-colors ease-in-out hover:bg-slate-950">
-            <Link to={`/commands/${route}`} className="flex items-center gap-x-4 p-3">
+            <NavLink to={`/commands/${route}`} className="flex items-center gap-x-4 p-3">
                 {icon}
                 <span className="text-2xl font-bold">{categoryName}</span>
-            </Link>
+            </NavLink>
         </li>
     )
 }
